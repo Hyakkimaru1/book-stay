a16cfad432fa9d268a105f0648debafafa8500e5
huudutg <huudutg@gmail.com>
2020-08-18 20:42:56 +0700

    chore rating dialog

--- backend/routes/user/user.route.js
+++ backend/routes/user/user.route.js
@@ -1,17 +1,15 @@
-const express = require('express');
-const userModels = require('../../models/user/user.models');
-const jwt = require('jsonwebtoken');
-const nodeMailer = require('../../models/user/sendMail.models')
-
+const express = require("express");
+const userModels = require("../../models/user/user.models");
+const jwt = require("jsonwebtoken");
+const nodeMailer = require("../../models/user/sendMail.models");
 
 //private key for token
-const privateKey = require('../../config/default.json').secret;
-var bcrypt = require('bcryptjs');
-var Cookies = require('js-cookie');
+const privateKey = require("../../config/default.json").secret;
+var bcrypt = require("bcryptjs");
+var Cookies = require("js-cookie");
 const router = express.Router();
 
-
-router.post('/profile', verifyToken, async(req, res) => {
+router.post("/profile", verifyToken, async(req, res) => {
     jwt.verify(req.token, privateKey, async(err, authData) => {
         if (err) {
             res.sendStatus(404);
@@ -25,26 +23,22 @@ router.post('/profile', verifyToken, async(req, res) => {
     });
 });
 
-router.post('/profile/update', verifyToken, async(req, res) => {
+router.post("/profile/update", verifyToken, async(req, res) => {
     jwt.verify(req.token, privateKey, async(err, authData) => {
         if (err) {
             res.sendStatus(404);
         } else {
             const row = await userModels.updateUser(req.body, authData.user.id);
             if (row.insertId !== null && row.insertId !== undefined) {
-                res.send('oke');
+                res.send("oke");
             } else res.sendStatus(400);
         }
     });
 });
 
-router.post('/login', async(req, res) => {
-
-
-
+router.post("/login", async(req, res) => {
     if (req.body) {
         // check is user or admin
-
 
         const row = await userModels.singleUsername(req.body.username);
         if (!row) {
@@ -62,19 +56,23 @@ router.post('/login', async(req, res) => {
                 if (err) {
                     res.send(500);
                 }
-                res.send({ token, id: row.id, ten: row.ten, email: row.email, sdt: row.sdt, avt: row.avatar });
+                res.send({
+                    token,
+                    id: row.id,
+                    ten: row.ten,
+                    email: row.email,
+                    sdt: row.sdt,
+                    avt: row.avatar,
+                });
             });
         } else res.sendStatus(403);
     } else {
         res.sendStatus(403);
     }
-
 });
 
-
-router.post('/signup', async(req, res) => {
+router.post("/signup", async(req, res) => {
     if (req.body) {
-
         const row = await userModels.singleUsername(req.body.email);
 
         if (row) {
@@ -86,7 +84,7 @@ router.post('/signup', async(req, res) => {
             const add = await userModels.addUser(entity);
             if (add) {
                 const row2 = await userModels.singleUsername(req.body.email);
-                const data = { id: row2.id }
+                const data = { id: row2.id };
                 jwt.sign({ user: data }, privateKey, function(err, token) {
                     res.send({ token, id: data.id });
                 });
@@ -95,12 +93,10 @@ router.post('/signup', async(req, res) => {
             }
         }
     } else res.sendStatus(403);
-
 });
 
-router.post('/forgotpw', async(req, res) => {
+router.post("/forgotpw", async(req, res) => {
     if (req.body) {
-
         const row = await userModels.singleUsername(req.body.email);
 
         if (!row) {
@@ -109,15 +105,11 @@ router.post('/forgotpw', async(req, res) => {
             const data = nodeMailer.sendOTP(req.body.email);
 
             res.send({ otp: data.otp });
-
         }
     } else res.sendStatus(403);
-
 });
 
-
-
-router.post('/resetpw', async(req, res) => {
+router.post("/resetpw", async(req, res) => {
     if (req.body) {
         console.log(req.body);
         const entity = req.body;
@@ -127,31 +119,38 @@ router.post('/resetpw', async(req, res) => {
         if (!row) {
             res.sendStatus(404);
         } else {
-
             res.send({ id: 1 });
-
         }
     } else res.sendStatus(403);
-
 });
 
-router.post('/loginAgain', verifyToken, async(req, res) => {
+router.post("/loginAgain", verifyToken, async(req, res) => {
     jwt.verify(req.token, privateKey, (err, authData) => {
         if (err) {
             res.sendStatus(404);
         } else {
-            res.send({ id: authData.user.id, ten: authData.user.ten, email: authData.user.email, sdt: authData.user.sdt, avt: authData.user.avt });
+            res.send({
+                id: authData.user.id,
+                ten: authData.user.ten,
+                email: authData.user.email,
+                sdt: authData.user.sdt,
+                avt: authData.user.avt,
+            });
         }
     });
 });
 
-router.post('/forgotpassword', async(req, res) => {
+router.post("/forgotpassword", async(req, res) => {
     const row = await userModels.getUserByEmail(req.body.email);
     if (row) {
         const currentDate = new Date();
         const countDownTime = new Date(currentDate.getTime() + 5 * 60000);
-        const randomnumber = Math.floor(Math.random() * (999999 - 100000 + 1)) + 100000;
-        const result = await userModels.addRecoveryKey({ user: row.id, keyRecovery: randomnumber });
+        const randomnumber =
+            Math.floor(Math.random() * (999999 - 100000 + 1)) + 100000;
+        const result = await userModels.addRecoveryKey({
+            user: row.id,
+            keyRecovery: randomnumber,
+        });
         const x = setTimeout(async() => {
             await userModels.removeRecoveryKey({ id: result.insertId });
         }, 4 * 60000);
@@ -159,14 +158,17 @@ router.post('/forgotpassword', async(req, res) => {
     } else res.sendStatus(404);
 });
 
-router.post('/confirmkey', async(req, res) => {
+router.post("/confirmkey", async(req, res) => {
     if (isNaN(req.body.key)) {
         res.sendStatus(400);
     } else {
         const row = await userModels.getRecoveryKey(req.body.id, req.body.key);
         if (row.length > 0) {
             userModels.removeRecoveryKey({ user: req.body.id });
-            jwt.sign({ id: req.body.id }, privateKey, { expiresIn: '1h' }, function(err, token) {
+            jwt.sign({ id: req.body.id }, privateKey, { expiresIn: "1h" }, function(
+                err,
+                token
+            ) {
                 if (err) {
                     res.send(500);
                 } else {
@@ -177,7 +179,7 @@ router.post('/confirmkey', async(req, res) => {
     }
 });
 
-router.post('/changepassword', async(req, res) => {
+router.post("/changepassword", async(req, res) => {
     jwt.verify(req.body.token, privateKey, async(err, authData) => {
         if (err) {
             res.sendStatus(404);
@@ -193,11 +195,6 @@ router.post('/changepassword', async(req, res) => {
     });
 });
 
-
-
-
-
-
 //verify the token
 function verifyToken(req, res, next) {
     //Get token in cookies
@@ -209,7 +206,7 @@ function verifyToken(req, res, next) {
     }
 }
 
-router.get('/mybooking/:id', verifyToken, async(req, res) => {
+router.get("/mybooking/:id", verifyToken, async(req, res) => {
     let id = req.params.id;
     jwt.verify(req.token, privateKey, async(err, authData) => {
         if (err) {
@@ -218,43 +215,27 @@ router.get('/mybooking/:id', verifyToken, async(req, res) => {
         } else {
             const row = await userModels.allBooking(req.params.id);
 
-
             if (row) {
                 res.send(row);
             } else res.sendStatus(400);
         }
     });
-
-
 });
 
-
-router.post('/mybooking/feedback', verifyToken, async(req, res) => {
-    console.log(req.body)
+router.post("/mybooking/feedback", verifyToken, async(req, res) => {
+    console.log(req.body);
     jwt.verify(req.token, privateKey, async(err, authData) => {
-
         if (err) {
             //console.log(req.body)
             res.sendStatus(404);
             console.log(err);
         } else {
-
             const row = await userModels.sendFeedback(req.body);
             if (row) {
-                res.send('oke');
+                res.send("oke");
             } else res.sendStatus(400);
         }
     });
 });
-
-
-
-
-
-
-
-
-
-
 
 module.exports = router;
--- front-end/src/components/MyBooking/MyBookingItem.js
+++ front-end/src/components/MyBooking/MyBookingItem.js
@@ -1,90 +1,92 @@
-import React, {useState} from 'react';
-import { ReactComponent as Heart } from '../../icons/icons8_heart_outline.svg';
-import { ReactComponent as Quick } from '../../icons/icons8_lightning_bolt_1.svg';
-import { ReactComponent as Star } from '../../icons/icons8_star.svg';
-import DiaglogRating from './DialogRating';
-import Rating from '@material-ui/lab/Rating';
-import { makeStyles } from '@material-ui/core/styles';
+import React, { useState } from "react";
+import { ReactComponent as Heart } from "../../icons/icons8_heart_outline.svg";
+import { ReactComponent as Quick } from "../../icons/icons8_lightning_bolt_1.svg";
+import { ReactComponent as Star } from "../../icons/icons8_star.svg";
+import DiaglogRating from "./DialogRating";
+import Rating from "@material-ui/lab/Rating";
+import { makeStyles } from "@material-ui/core/styles";
 // import Button from '@material-ui/core/Button';
 
 const MyBookingItem = (props) => {
-    const [rating, setRating] = useState(false)
-    const ratingClick = (a) => {
-        setRating(a);
-    }
+  const [rating, setRating] = useState(false);
+  const ratingClick = (a) => {
+    setRating(a);
+  };
 
-
-
-    return (
-
-        <li className="row">
-            <div className="col-3-of-4">
-                <div className="item">
-                    <div className="item__picture">
-                        <div className="item__picture--src">
-                            <img width="215" height="155" src="https://cdn.luxstay.com/users/170204/vopvKF2uSyomu130qFjYS0Nt.jpg">
-                            </img>
-                        </div>
-                        <div className="item__picture--heart">
-                            <Heart />
-                        </div>
-
-                    </div>
-                    <div className="item__info">
-                        <div className="item__info--type">
-                            CĂN HỘ DỊCH VỤ
-                                </div>
-                        <div className="item__info--name">
-                            <a href="" className="item__title">
-                                <div className="item__title--icon">
-                                    <Quick />
-                                </div>
-                                <span class="item__title--contain">{props.name}</span>
-                            </a>
-                        </div>
-                        <div className="item__info--subcontent">
-                            <div className="subcontent__left">
-                                <div className="subcontent__left--location">
-                                    <a>{props.diaChi}</a>
-
-                                </div>
-                                <div className="subcontent__left--room">
-                                   {props.soPhong? props.soPhong:1} phòng
-                                        </div>
-                                <div className="subcontent__left--rating">
-                                <Rating name="half-rating-read" defaultValue={2.5} precision={0.5} readOnly />
-                                <button type="" class="bt__default" id="changeAvt" onClick={()=> ratingClick(true)} >Đánh giá</button>
-                                <DiaglogRating id={props.id} onChange={rating} onClose={()=> ratingClick(false)} name={props.name}></DiaglogRating>
-                                </div>
-
-                            </div>
-                            
-                             <div className="subcontent__right">
-                                <div className="subcontent__right--price">
-                                    <div className="subcontent__right--price-sub">
-                                        <p>Giá cho một đêm</p>
-                                    </div>
-                                    <div className="subcontent__right--price-num">
-                                        <span>{props.gia}</span>
-                                    </div>
-
-                                </div>
-
-
-
-                            </div>
-
-                        </div>
-
-                    </div>
-
-                </div>
+  return (
+    <li className="row">
+      <div className="col-3-of-4">
+        <div className="item">
+          <div className="item__picture">
+            <div className="item__picture--src">
+              <img
+                width="215"
+                height="155"
+                src="https://cdn.luxstay.com/users/170204/vopvKF2uSyomu130qFjYS0Nt.jpg"
+              ></img>
             </div>
+            <div className="item__picture--heart">
+              <Heart />
+            </div>
+          </div>
+          <div className="item__info">
+            <div className="item__info--type">CĂN HỘ DỊCH VỤ</div>
+            <div className="item__info--name">
+              <a href="" className="item__title">
+                <div className="item__title--icon">
+                  <Quick />
+                </div>
+                <span class="item__title--contain">{props.name}</span>
+              </a>
+            </div>
+            <div className="item__info--subcontent">
+              <div className="subcontent__left">
+                <div className="subcontent__left--location">
+                  <a>{props.diaChi}</a>
+                </div>
+                <div className="subcontent__left--room">
+                  {props.soPhong ? props.soPhong : 1} phòng
+                </div>
+                <div className="subcontent__left--rating">
+                  <Rating
+                    name="half-rating-read"
+                    defaultValue={2.5}
+                    precision={0.5}
+                    readOnly
+                  />
+                  <button
+                    type=""
+                    class="bt__default"
+                    id="changeAvt"
+                    onClick={() => ratingClick(true)}
+                  >
+                    Đánh giá
+                  </button>
+                  <DiaglogRating
+                    id={props.id}
+                    onChange={rating}
+                    onClose={() => ratingClick(false)}
+                    name={props.name}
+                  ></DiaglogRating>
+                </div>
+              </div>
 
-
-        </li>
-
-    );
-}
+              <div className="subcontent__right">
+                <div className="subcontent__right--price">
+                  <div className="subcontent__right--price-sub">
+                    <p>Giá cho một đêm</p>
+                  </div>
+                  <div className="subcontent__right--price-num">
+                    <span>{props.gia}</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </li>
+  );
+};
 
 export default MyBookingItem;
